683ac8b052fb6cb686b36c1263b621308ba470cb
AveryNewhart <newhartreeceavery@gmail.com>
2023-02-11 14:35:06 -0500

    fixed error when seeding

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
-require('dotenv').config(); // requires / gets dotenv for us to use.
+const Sequelize = require('sequelize');
 
-const Sequelize = require('sequelize'); // requiring and getting sequelize for us to use.
+require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+   {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
